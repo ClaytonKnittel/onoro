e38d01837900f6df5ec77f8b37b2daea211e6488
claytonknittel <cknittel@wustl.edu>
2022-09-27 01:53:30 -0700

    print neighbors

--- include/onoro.h
+++ include/onoro.h
@@ -1,12 +1,22 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
+#include <utility>
 
 namespace Onoro {
 
 class Game {
  public:
+  // (x, y) coordinates as an index.
+  typedef std::pair<uint32_t, uint32_t> idx_t;
+  // coordinates in vector space
+  typedef std::pair<uint32_t, uint32_t> pos_t;
+
   Game(uint32_t n_pawns);
+  Game(const Game&, idx_t move);
+
+  std::string Print() const;
 
  private:
   enum class TileState {
@@ -14,20 +24,140 @@ class Game {
     TILE_BLACK = 1,
     TILE_WHITE = 2,
   };
+  // bits per entry in the board
+  static constexpr uint32_t bits_per_entry = 64;
   static constexpr uint32_t bits_per_tile = 2;
+  static constexpr uint64_t tile_bitmask = (1 << bits_per_tile) - 1;
+  static constexpr uint32_t max_pawns_per_player = 8;
+  static constexpr uint32_t min_neighbors_per_pawn = 2;
+
+  struct GameState {
+    uint8_t blackTurn : 1;
+    // You can play this game with a max of 8 pawns
+    uint8_t pawnsInStock : 3;
+    uint8_t __reserved   : 7;
+  };
 
   uint64_t* board_;
   // Width/height of the board
   uint32_t board_len_;
+
+  GameState state_;
+
+  // Sum of all pos_t's of pieces on the board
+  pos_t sum_of_mass_;
+
+  // Min x/y index values of any piece
+  idx_t min_idx_;
+  // Max x/y index values of any piece
+  idx_t max_idx_;
+
+  /*
+   * Converts an absolute index to an idx_t.
+   */
+  idx_t toIdx(uint32_t i) const;
+
+  /*
+   * Converts an idx_t to an absolute index.
+   */
+  uint32_t fromIdx(idx_t idx) const;
 
   /*
    * Calculates the required size of board for a game with n_pawns pawns.
    */
   static uint32_t calcBoardSize(uint32_t n_pawns);
 
+  TileState getTile(idx_t idx) const;
+
+  void setTile(idx_t idx, TileState);
+
   template <class CallbackFnT>
-  void forEachNeighbor(uint32_t idx, CallbackFnT cb) {
-    uint32_t
+  void forEachNeighbor(idx_t idx, CallbackFnT cb) const {
+    auto [x, y] = idx;
+
+    if (y > 0) {
+      cb({ x, y - 1 });
+
+      if (y % 2 == 0) {
+        if (x < board_len_ - 1) {
+          cb({ x + 1, y - 1 });
+        }
+      } else {
+        if (x > 0) {
+          cb({ x - 1, y - 1 });
+        }
+      }
+    }
+    if (x > 0) {
+      cb({ x - 1, y });
+    }
+    if (x < board_len_ - 1) {
+      cb({ x + 1, y });
+    }
+    if (y < board_len_ - 1) {
+      cb({ x, y + 1 });
+
+      if (y % 2 == 0) {
+        if (x < board_len_ - 1) {
+          cb({ x + 1, y + 1 });
+        }
+      } else {
+        if (x > 0) {
+          cb({ x - 1, y + 1 });
+        }
+      }
+    }
+  }
+
+  template <class CallbackFnT>
+  void forEachMove(CallbackFnT cb) const {
+    static constexpr uint32_t tmp_board_tile_bits = 2;
+    static constexpr uint64_t tmp_board_tile_mask =
+        (uint64_t(1) << tmp_board_tile_bits) - 1;
+
+    uint64_t tmp_board_len =
+        (board_len_ * board_len_ * tmp_board_tile_bits + bits_per_entry - 1) /
+        bits_per_entry;
+    // bitvector of moves already taken
+    uint64_t tmp_board[tmp_board_len];
+    memset(tmp_board, 0, tmp_board_len * 8);
+
+    for (uint64_t i = 0; i < tmp_board_len; i++) {
+      uint64_t board_bitmask = board_[i];
+      uint64_t bitmask_idx_off = i * (bits_per_entry / bits_per_tile);
+
+      while (board_bitmask != 0) {
+        // a tile will have one of its bits set, so find any bit in its bit set,
+        // then divide by size (relying on remainder truncation) to find its
+        // index relative to the bitmask
+        uint32_t next_idx_off = __builtin_ctzl(board_bitmask) / bits_per_tile;
+        uint32_t next_idx = next_idx_off + bitmask_idx_off;
+        board_bitmask =
+            board_bitmask & ~(tile_bitmask << (bits_per_tile * next_idx));
+
+        forEachNeighbor(toIdx(next_idx), [&tmp_board, this,
+                                          &cb](idx_t neighbor_idx) {
+          if (getTile(neighbor_idx) == TileState::TILE_EMPTY) {
+            uint32_t idx = fromIdx(neighbor_idx);
+            uint32_t tb_shift = tmp_board_tile_bits *
+                                (idx % (bits_per_entry / tmp_board_tile_bits));
+            uint64_t tbb =
+                tmp_board[idx / (bits_per_entry / tmp_board_tile_bits)];
+            uint64_t mask = tmp_board_tile_mask << tb_shift;
+            uint64_t full_mask = uint64_t(min_neighbors_per_pawn) << tb_shift;
+
+            if ((tbb & mask) != full_mask) {
+              tbb += (uint64_t(1) << tb_shift);
+              tmp_board[idx / (bits_per_entry / tmp_board_tile_bits)] = tbb;
+
+              if ((tbb & mask) == full_mask) {
+                cb(neighbor_idx);
+              }
+            }
+          }
+        });
+      }
+    }
   }
 };
 }  // namespace Onoro
--- src/onoro.cc
+++ src/onoro.cc
@@ -1,7 +1,13 @@
 
 #include "onoro.h"
 
+#include <sstream>
+
 namespace Onoro {
+
+Game::idx_t operator+(const Game::idx_t& a, const Game::idx_t& b) {
+  return { a.first + b.first, a.second + b.second };
+}
 
 /*
  * The game played on a hexagonal grid, but is internally represented as a 2D
@@ -16,12 +22,116 @@ namespace Onoro {
  *  3n  3n+1   3n+2  3n+3  3n+4    ...
  */
 
-Game::Game(uint32_t n_pawns) : board_size_(calcBoardSize(n_pawns)) {
-  return (board_len_ * board_len_ * bits_per_tile + 63) / 64;
-  this->board_ = new uint64_t[board_size_];
+// Black goes first, but since black has 2 forced moves and white only has 1,
+// white is effectively first to make a choice.
+Game::Game(uint32_t n_pawns)
+    : board_len_(calcBoardSize(n_pawns)),
+      state_({ 0, static_cast<uint8_t>(n_pawns), 0 }) {
+  if (n_pawns > 2 * max_pawns_per_player) {
+    throw new std::runtime_error("Cannot play with more than 16 pawns");
+  }
+
+  uint64_t board_size =
+      (board_len_ * board_len_ * bits_per_tile + bits_per_entry - 1) /
+      bits_per_entry;
+  this->board_ = new uint64_t[board_size]();
+
+  uint32_t mid_idx = (board_len_ - 1) / 2;
+  idx_t b_start = { mid_idx, mid_idx };
+  idx_t w_start = { mid_idx, mid_idx + 1 };
+  idx_t b_next = { mid_idx + 1, mid_idx };
+
+  setTile(b_start, TileState::TILE_BLACK);
+  setTile(w_start, TileState::TILE_WHITE);
+  setTile(b_next, TileState::TILE_BLACK);
+
+  min_idx_ = b_start;
+  max_idx_ = { mid_idx + 1, mid_idx + 1 };
+  sum_of_mass_ = b_start + w_start + b_next;
+
+  printf("%s\n", this->Print().c_str());
+
+  forEachMove([this](idx_t idx) {
+    Game g2(*this, idx);
+    printf("Move: (%u, %u)\n", idx.first, idx.second);
+    printf("%s\n", g2.Print().c_str());
+  });
+}
+
+Game::Game(const Game& g, idx_t move)
+    : board_len_(g.board_len_),
+      state_(
+          { !g.state_.blackTurn,
+            static_cast<uint8_t>(g.state_.pawnsInStock - !g.state_.blackTurn),
+            g.state_.__reserved }) {
+  uint64_t board_size =
+      (board_len_ * board_len_ * bits_per_tile + bits_per_entry - 1) /
+      bits_per_entry;
+  board_ = new uint64_t[board_size];
+  memcpy(board_, g.board_, board_size * sizeof(uint64_t));
+
+  setTile(move,
+          g.state_.blackTurn ? TileState::TILE_BLACK : TileState::TILE_WHITE);
+}
+
+std::string Game::Print() const {
+  static const char tile_str[3] = {
+    '.',
+    'B',
+    'W',
+  };
+
+  std::ostringstream ostr;
+  for (uint32_t y = 0; y < board_len_; y++) {
+    if (y % 2 == 0) {
+      ostr << " ";
+    }
+
+    for (uint32_t x = 0; x < board_len_; x++) {
+      ostr << tile_str[static_cast<int>(getTile({ x, y }))];
+      if (x < board_len_ - 1) {
+        ostr << " ";
+      }
+    }
+
+    if (y < board_len_ - 1) {
+      ostr << "\n";
+    }
+  }
+  return ostr.str();
+}
+
+Game::idx_t Game::toIdx(uint32_t i) const {
+  return { i % board_len_, i / board_len_ };
+}
+
+uint32_t Game::fromIdx(Game::idx_t idx) const {
+  return idx.first + idx.second * board_len_;
 }
 
 uint32_t Game::calcBoardSize(uint32_t n_pawns) {
   return n_pawns;
+}
+
+Game::TileState Game::getTile(idx_t idx) const {
+  auto [x, y] = idx;
+  uint32_t i = x + y * board_len_;
+
+  uint64_t tile_bitv = board_[i * bits_per_tile / bits_per_entry];
+  uint32_t bitv_idx = i % (bits_per_entry / bits_per_tile);
+
+  return TileState((tile_bitv >> (bits_per_tile * bitv_idx)) & tile_bitmask);
+}
+
+void Game::setTile(idx_t idx, TileState piece) {
+  auto [x, y] = idx;
+  uint32_t i = x + y * board_len_;
+
+  uint64_t tile_bitv = board_[i * bits_per_tile / bits_per_entry];
+  uint32_t bitv_idx = i % (bits_per_entry / bits_per_tile);
+  uint64_t bitv_mask =
+      (static_cast<uint64_t>(piece) << (bits_per_tile * bitv_idx));
+
+  board_[i * bits_per_tile / bits_per_entry] = tile_bitv | bitv_mask;
 }
 }  // namespace Onoro
