4e0ce16b2ce8e64a60b103bca7c126e98c8e6831
claytonknittel <cknittel@wustl.edu>
2022-10-02 19:10:15 -0700

    benchmark with time

--- src/main.cc
+++ src/main.cc
@@ -9,8 +9,74 @@ static constexpr bool print_overwrite = false;
 
 void TestUnionFind();
 
+static double timespec_diff(struct timespec* start, struct timespec* end) {
+  return (end->tv_sec - start->tv_sec) +
+         (((double) (end->tv_nsec - start->tv_nsec)) / 1000000000.);
+}
+
+static int benchmark() {
+  Onoro::Game<n_pawns> g;
+
+  static constexpr uint32_t n_moves = 60000;
+
+  for (uint32_t i = 0; i < n_pawns * 2 - 3; i++) {
+    uint32_t move_cnt = 0;
+    g.forEachMove([&move_cnt](Onoro::idx_t idx) {
+      move_cnt++;
+      return true;
+    });
+
+    int which = rand() % move_cnt;
+    g.forEachMove([&g, &which](Onoro::idx_t idx) {
+      if (which == 0) {
+        Onoro::Game<n_pawns> g2(g, idx);
+        g = std::move(g2);
+        return false;
+      } else {
+        which--;
+        return true;
+      }
+    });
+  }
+
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  for (uint32_t i = 0; i < n_moves; i++) {
+    int move_cnt = 0;
+    g.forEachMoveP2([&move_cnt](Onoro::idx_t to, Onoro::idx_t from) {
+      move_cnt++;
+      return true;
+    });
+
+    if (move_cnt == 0) {
+      printf("Player won by no legal moves\n");
+      return -1;
+    }
+
+    int which = rand() % move_cnt;
+    g.forEachMoveP2([&g, &which](Onoro::idx_t to, Onoro::idx_t from) {
+      if (which == 0) {
+        Onoro::Game<n_pawns> g2(g, to, from);
+        g = std::move(g2);
+        return false;
+      } else {
+        which--;
+        return true;
+      }
+    });
+  }
+  clock_gettime(CLOCK_MONOTONIC, &end);
+
+  printf("Did %u moves in %f s\n", n_moves, timespec_diff(&start, &end));
+
+  return 0;
+}
+
 int main(int argc, char* argv[]) {
-  srand(time(NULL));
+  srand(0);
+
+  return benchmark();
 
   Onoro::Game<n_pawns> g;
 
--- src/test_union_find.cc
+++ src/test_union_find.cc
@@ -7,7 +7,7 @@
 
 #include "union_find.h"
 
-double timespec_diff(struct timespec* start, struct timespec* end) {
+static double timespec_diff(struct timespec* start, struct timespec* end) {
   return (end->tv_sec - start->tv_sec) +
          (((double) (end->tv_nsec - start->tv_nsec)) / 1000000000.);
 }
