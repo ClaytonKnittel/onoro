a0f4d725919b98e8cd7eedf7c39b866eace498ad
claytonknittel <cknittel@wustl.edu>
2022-12-06 01:55:49 -0800

    complete onoro, minus win checking

--- pylib/init.py
+++ pylib/init.py
@@ -1,18 +1,19 @@
 
-from typing import Iterable, List
+import random
+from typing import Iterable, List, Set, Tuple
 
 from game_state_pb2 import GameState
 
 Pawn = GameState.Pawn
 
-def pawn_neighbors(pawn: Pawn) -> Iterable[Pawn]:
+def coord_neighbors(pawn: Tuple[int, int]) -> Iterable[Tuple[int, int]]:
   return (
-      Pawn(x=pawn.x + 1, y=pawn.y),
-      Pawn(x=pawn.x + 1, y=pawn.y + 1),
-      Pawn(x=pawn.x, y=pawn.y + 1),
-      Pawn(x=pawn.x - 1, y=pawn.y),
-      Pawn(x=pawn.x - 1, y=pawn.y - 1),
-      Pawn(x=pawn.x, y=pawn.y - 1),
+      (pawn[0] + 1, pawn[1]),
+      (pawn[0] + 1, pawn[1] + 1),
+      (pawn[0], pawn[1] + 1),
+      (pawn[0] - 1, pawn[1]),
+      (pawn[0] - 1, pawn[1] - 1),
+      (pawn[0], pawn[1] - 1),
     )
 
 class Onoro:
@@ -70,25 +71,71 @@ class Onoro:
         res += '\n'
     return res
 
-  def P1Moves(self) -> List[Pawn]:
-    if len(self.pawns) >= self.num_pawns:
-      raise RuntimeError('Not phase 1, %d pawns in play' % (len(self.num_pawns)))
-
-    pawns = {(pawn.x, pawn.y) for pawn in self.pawns}
+  def PlayableSpots(self, pawns: Set[Tuple[int, int]]) -> Iterable[Tuple[int, int]]:
     counts = dict()
-    for pawn in self.pawns:
-      for loc in pawn_neighbors(pawn):
-        coord = (loc.x, loc.y)
+    for pawn in pawns:
+      for coord in coord_neighbors(pawn):
         if coord in pawns:
           continue
         if coord not in counts:
           counts[coord] = 0
         counts[coord] += 1
     counts = {loc: count for loc, count in counts.items() if count >= 2}
-    return [Pawn(x=pawn[0], y=pawn[1]) for pawn in counts.keys()]
+    return counts.keys()
+
+  def _RemoveAdjacent(self, pawn: Tuple[int, int], pawns: Set[Tuple[int, int]]) -> Set[Tuple[int, int]]:
+    for neighbor in coord_neighbors(pawn):
+      if neighbor in pawns:
+        pawns = pawns - {neighbor}
+        pawns = self._RemoveAdjacent(neighbor, pawns)
+    return pawns
+
+  def Connected(self, pawns: Set[Tuple[int, int]]) -> bool:
+    if not pawns:
+      return True
+    pawn = pawns.pop()
+    return not self._RemoveAdjacent(pawn, pawns)
+
+  def TwoNeighborsEach(self, pawns: Set[Tuple[int, int]]) -> bool:
+    for pawn in pawns:
+      n_neighbors = len([neighbor for neighbor in coord_neighbors(pawn) if neighbor in pawns])
+      if n_neighbors < 2:
+        return False
+    return True
+
+  def P1Moves(self) -> Iterable[Pawn]:
+    if len(self.pawns) >= self.num_pawns:
+      raise RuntimeError('Not phase 1, %d pawns in play' % (len(self.num_pawns)))
+
+    pawns = {(pawn.x, pawn.y) for pawn in self.pawns}
+    return (Pawn(x=pawn[0], y=pawn[1], black=self.black_turn) for pawn in self.PlayableSpots(pawns))
 
   def MakeP1Move(self, move: Pawn) -> None:
-    self.pawns += [Pawn(x=move.x, y=move.y, black=self.black_turn),]
+    self.pawns += [move,]
+    self.black_turn = not self.black_turn
+
+  def P2Moves(self) -> Iterable[Tuple[Pawn, Pawn]]:
+    if len(self.pawns) != self.num_pawns:
+      raise RuntimeError('Not phase 2, %d pawns in play' % (len(self.num_pawns)))
+
+    pawns = {(pawn.x, pawn.y) for pawn in self.pawns}
+    for pawn in self.pawns:
+      if pawn.black != self.black_turn:
+        continue
+
+      pawn_coord = (pawn.x, pawn.y)
+      rem_pawns = pawns - {pawn_coord}
+      for coord in self.PlayableSpots(rem_pawns):
+        if coord == pawn_coord:
+          continue
+
+        new_pawns = rem_pawns | {coord}
+        if self.Connected(new_pawns) and self.TwoNeighborsEach(new_pawns):
+          yield (pawn, Pawn(x=coord[0], y=coord[1], black=pawn.black))
+
+  def MakeP2Move(self, move: Tuple[Pawn, Pawn]) -> None:
+    self.pawns.remove(move[0])
+    self.pawns += (move[1],)
     self.black_turn = not self.black_turn
 
 def deserialize(gs: GameState, num_pawns: int) -> Onoro:
@@ -101,6 +148,8 @@ def deserialize(gs: GameState, num_pawns: int) -> Onoro:
   return Onoro(num_pawns, gs.pawns, gs.black_turn)
 
 def main():
+  random.seed(1)
+  n_pawns = 6
   pawns = (
       Pawn(x=1, y=1, black=True),
       Pawn(x=1, y=2, black=False),
@@ -108,13 +157,28 @@ def main():
     )
   gs = GameState(pawns=pawns,
                  black_turn=False, turn_num=2, finished=False)
-  game = deserialize(gs, 16)
+  game = deserialize(gs, n_pawns)
   print(game)
 
-  for move in game.P1Moves():
-    g = deserialize(gs, 16)
+  for i in range(3, n_pawns):
+    g = deserialize(gs, n_pawns)
+    moves = list(g.P1Moves())
+    move = random.choice(moves)
+
     g.MakeP1Move(move)
     print(g)
+
+    gs = g.serialize()
+
+  for i in range(n_pawns, n_pawns + 5):
+    g = deserialize(gs, n_pawns)
+    moves = list(g.P2Moves())
+    move = random.choice(moves)
+
+    g.MakeP2Move(move)
+    print(g)
+
+    gs = g.serialize()
 
 if __name__ == '__main__':
   main()
